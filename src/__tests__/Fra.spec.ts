23a2a8de2bee060d956d8c3294c8eee85a2a85ef
Patrick Dunn <patwritescode@users.noreply.github.com>
2018-06-17 01:09:54 -0400

    add tests

--- src/__tests__/Fra.spec.ts
+++ src/__tests__/Fra.spec.ts
@@ -5,17 +5,26 @@ describe("Fra", () => {
         public lastName: string = "Dunn";
     }
     // tslint:disable-next-line:max-classes-per-file
+    class HumanDTO {
+        public age: number = 28;
+    }
+    // tslint:disable-next-line:max-classes-per-file
     class Person {
         public firstName: string = undefined;
         public lastName: string = undefined;
         public fullName: string = undefined;
+        public age: number = undefined;
     }
     let source: PersonDTO = null;
+    let secondSource: HumanDTO = null;
     let mapper: Fra<PersonDTO, Person> = null;
+    let secondMapper: Fra<HumanDTO, Person> = null;
     beforeEach(() => {
         source = new PersonDTO();
+        secondSource = new HumanDTO();
         mapper = Fra.createMap<PersonDTO, Person>(Person)
             .field("fullName", (src) => `${src.firstName} ${src.lastName}`);
+        secondMapper = Fra.createMap<HumanDTO, Person>(Person);
     });
     it("automaps properties", () => {
         const result = mapper.map(source);
@@ -29,5 +38,18 @@ describe("Fra", () => {
     it("maps all", () => {
         const result = mapper.mapAll([source]);
         expect(result).toHaveLength(1);
+    });
+    it("can link mapeprs", () => {
+        const linkedMapper = link(Person, [mapper, secondMapper]);
+        const result = linkedMapper(source, secondSource);
+        expect(result.firstName).toEqual(source.firstName);
+        expect(result.age).toEqual(secondSource.age);
+    });
+    it("can map to existing", () => {
+        const person = new Person();
+        person.age = 28;
+        const result = mapper.map(source, person);
+        expect(result.age).toEqual(28);
+        expect(result.firstName).toEqual(source.firstName);
     });
 });
